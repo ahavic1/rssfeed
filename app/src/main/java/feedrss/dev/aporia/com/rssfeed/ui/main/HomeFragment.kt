ea17a7b2c767d4b8286d0cdffd97ca833af6f544
Ademir <ahavic1@users.noreply.github.com>
2019-08-24 18:39:24 +0200

    Refactor HomeFragment - Code cleanup

--- app/src/main/java/feedrss/dev/aporia/com/rssfeed/ui/main/HomeFragment.kt
+++ app/src/main/java/feedrss/dev/aporia/com/rssfeed/ui/main/HomeFragment.kt
@@ -1,13 +1,11 @@
 package feedrss.dev.aporia.com.rssfeed.ui.main
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import feedrss.dev.aporia.com.rssfeed.ui.base.OnFragmentInteractionListener
 import feedrss.dev.aporia.com.rssfeed.R
 import feedrss.dev.aporia.com.rssfeed.common.replaceFragment
 import feedrss.dev.aporia.com.rssfeed.ui.main.feed.AddFeedFragment
@@ -17,27 +15,17 @@ import feedrss.dev.aporia.com.rssfeed.ui.main.post.PostsFragment
 
 
 class HomeFragment : Fragment() {
-    private var listener: OnFragmentInteractionListener? = null
-    private lateinit var bottomNavBar: BottomNavigationView
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val v = inflater.inflate(R.layout.fragment_home, container, false)
-        bottomNavBar = v.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        return v
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setOnNavBarClickListener()
+        setOnNavBarClickListener(view.findViewById(R.id.bottomNavigationView))
     }
 
-    private fun setOnNavBarClickListener() {
+    private fun setOnNavBarClickListener(bottomNavBar: BottomNavigationView) {
         bottomNavBar.setOnNavigationItemSelectedListener {
             val selectedFragment = when (it.itemId) {
                 R.id.actionHome -> PostsFragment.newInstance()
@@ -53,21 +41,6 @@ class HomeFragment : Fragment() {
         }
         bottomNavBar.selectedItemId = R.id.actionHome
     }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
 
     companion object {
         @JvmStatic
--- app/src/main/java/feedrss/dev/aporia/com/rssfeed/ui/main/MainActivity.kt
+++ app/src/main/java/feedrss/dev/aporia/com/rssfeed/ui/main/MainActivity.kt
@@ -7,20 +7,11 @@ import androidx.navigation.findNavController
 import feedrss.dev.aporia.com.rssfeed.ui.base.OnFragmentInteractionListener
 import feedrss.dev.aporia.com.rssfeed.R
 
-class MainActivity : AppCompatActivity(),
-    OnFragmentInteractionListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-//        NavigationUI.setupWithNavController(bottomNav, findNavController(R.id.navHostFragment))
-//        bottomNav.menu.getItem(1).isChecked = true
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
-
-    override fun onFragmentInteraction(action: NavDirections) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
